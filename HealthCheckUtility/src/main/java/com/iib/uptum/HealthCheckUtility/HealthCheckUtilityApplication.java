2ab3b32edb607054d77e5715a6492452a06bb10d
Prabhu <Prabhu@DESKTOP-JSI5KDN>
2020-10-26 17:39:29 +0530

    rrrr

---
+++ HealthCheckUtility/src/main/java/com/iib/uptum/HealthCheckUtility/HealthCheckUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.iib.uptum.HealthCheckUtility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckUtilityApplication.class, args);
+	}
+
+}
---
+++ HealthCheckUtility/src/test/java/com/iib/uptum/HealthCheckUtility/HealthCheckUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iib.uptum.HealthCheckUtility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
